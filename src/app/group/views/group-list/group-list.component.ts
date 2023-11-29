4047541ff680362f979db83094a393cfcbdbc407
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-11-29 14:40:47 +0100

    Corregido altura de las filas y filtros

--- src/app/group/views/group-list/group-list.component.ts
+++ src/app/group/views/group-list/group-list.component.ts
@@ -9,29 +9,56 @@ import { GroupService } from 'src/app/group/services/group.service';
   selector: 'app-group-list',
   templateUrl: './group-list.component.html',
   styleUrls: ['./group-list.component.scss'],
-  providers: [DialogService, DynamicDialogRef, DynamicDialogConfig,ConfirmationService]
+  providers: [DialogService, DynamicDialogRef, DynamicDialogConfig, ConfirmationService]
 })
 export class GroupListComponent implements OnInit {
-  groups: Group[];
+
+  groups: Group[] = [
+    {
+      id: 1,
+      name: 'Group 1',
+      managers: 'Manager 1',
+      members: 10,
+      subgroups: 2,
+      externalId: 'ext1'
+    },
+    {
+      id: 2,
+      name: 'Group 2',
+      managers: 'Manager 2',
+      members: 15,
+      subgroups: 3,
+      externalId: 'ext2'
+    },
+    {
+      id: 3,
+      name: 'Group 3',
+      managers: 'Manager 3',
+      members: 8,
+      subgroups: 1,
+      externalId: 'ext3'
+    }];
+    
   adminView: boolean = false;
+
   constructor(
     private ref: DynamicDialogRef,
     private dialogService: DialogService,
     private groupService: GroupService,
   ) { }
 
-  ngOnInit(): void {  
-    this.adminView=false; 
-    this.getAllGroups();        
+  ngOnInit(): void {
+    this.adminView = false;
+    this.getAllGroups();
   }
 
-  clickAdminView(e) {    
+  clickAdminView(e) {
     const adminView = e.checked;
-     if (adminView) {
+    if (adminView) {
       this.getAllGroupsAdmin();
-     }else{
-      this.getAllGroups(); 
-     }
+    } else {
+      this.getAllGroups();
+    }
   }
 
   getAllGroupsAdmin() {
@@ -41,7 +68,7 @@ export class GroupListComponent implements OnInit {
       },
     });
   }
-  
+
   getAllGroups() {
     this.groupService.getAllGroups().subscribe({
       next: (res: Group[]) => {
@@ -50,23 +77,23 @@ export class GroupListComponent implements OnInit {
     });
   }
 
-  editGroup(groupEdit:Group){
-    this.ref = this.dialogService.open(GroupEditComponent,{
-      height:'calc(100vh - 1px)',
-      width:'1200px',
+  editGroup(groupEdit: Group) {
+    this.ref = this.dialogService.open(GroupEditComponent, {
+      height: 'calc(100vh - 1px)',
+      width: '1200px',
       baseZIndex: 10000,
       contentStyle: { overflow: 'auto' },
-      data:{
+      data: {
         group: groupEdit
       },
-      closable:false
+      closable: false
     });
     this.onClose();
   }
 
-  onClose():void{
+  onClose(): void {
     this.ref.onClose.subscribe(
-      (results:any) => {
+      (results: any) => {
         this.ngOnInit();
       }
     )
