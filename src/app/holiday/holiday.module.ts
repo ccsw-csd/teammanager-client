6a635da4edc69210bd1735c7cc587bfbef36f2a5
ivanmr14 <imitro@alumni.uv.es>
2023-12-18 13:51:34 +0100

    Listado de centros implementado

--- src/app/holiday/holiday.module.ts
+++ src/app/holiday/holiday.module.ts
@@ -3,7 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HolidayListComponent } from './views/holiday-list/holiday-list.component';
 import { HolidayEditComponent } from './views/holiday-edit/holiday-edit.component';
 
-
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DynamicDialogModule } from "primeng/dynamicdialog";
+import { ConfirmDialogModule } from "primeng/confirmdialog";
+import { ListboxModule } from 'primeng/listbox';
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from "primeng/tooltip";
+import { InputTextModule } from "primeng/inputtext";
+import { PaginatorModule } from "primeng/paginator";
+import { TabViewModule } from 'primeng/tabview';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 @NgModule({
   declarations: [
@@ -11,7 +23,22 @@ import { HolidayEditComponent } from './views/holiday-edit/holiday-edit.componen
     HolidayEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AutoCompleteModule,
+    ListboxModule,
+    TableModule,
+    ToastModule,
+    DynamicDialogModule,
+    ButtonModule,
+    TooltipModule,
+    ConfirmDialogModule,
+    PaginatorModule,
+    InputTextModule,
+    TooltipModule,
+    ToastModule,
+    TabViewModule,
+    InputSwitchModule,
+    ScrollPanelModule,
   ]
 })
 export class HolidayModule { }
--- src/app/holiday/views/holiday-list/holiday-list.component.ts
+++ src/app/holiday/views/holiday-list/holiday-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { DialogService, DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
+import { Center } from 'src/app/holiday/model/Center';
+import { HolidayService } from '../../holiday.service';
 
 @Component({
   selector: 'app-holiday-list',
   templateUrl: './holiday-list.component.html',
-  styleUrls: ['./holiday-list.component.scss']
+  styleUrls: ['./holiday-list.component.scss'],
+  providers: [DialogService, DynamicDialogRef, DynamicDialogConfig,ConfirmationService]
 })
 export class HolidayListComponent implements OnInit {
 
-  constructor() { }
+  centers: Center[];
+  constructor(
+    private ref: DynamicDialogRef,
+    private dialogService: DialogService,
+    private holidayService: HolidayService,
+  ) { }
 
   ngOnInit(): void {
+    this.getAllCenters();
+  }
+
+  getAllCenters() {
+    this.holidayService.getAllCenters().subscribe({
+      next: (res: Center[]) => {
+        this.centers = res;
+      },
+    });
+
   }
 
 }
