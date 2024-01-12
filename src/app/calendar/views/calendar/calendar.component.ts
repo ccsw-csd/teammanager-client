8cce12544a8b613482239552e945e3b50940cb0d
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2024-01-12 10:52:39 +0100

    Eliminado cambios en calendar

--- src/app/calendar/views/calendar/calendar.component.ts
+++ src/app/calendar/views/calendar/calendar.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewChild, ElementRef, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { ScheduleType } from '../model/schedule-type';
 import { MetadataDay } from '../model/metadata-day';
 import { DropdownEntry } from '../model/dropdown-entry';
-import { Festive } from 'src/app/holiday/model/Festive';
-import { DialogService, DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
+
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ConfirmationService } from 'primeng/api';
-import { CalendarService } from '../../calendar.service';
 
 @Component({
   selector: 'app-edit-calendar',
@@ -13,11 +12,9 @@ import { CalendarService } from '../../calendar.service';
   styleUrls: ['./calendar.component.scss'],
   providers: [DynamicDialogRef, ConfirmationService],
 })
-export class CalendarComponent{
+export class CalendarComponent {
   @ViewChild('calendarsDiv') calendarsDiv: ElementRef;
 
-
-  @Output() clickEvent = new EventEmitter<MetadataDay>();
   //ATRIBUTOS
 
  
@@ -29,30 +26,12 @@ export class CalendarComponent{
 
   years: DropdownEntry[] = [];
   calendars: Map<String, Map<String, MetadataDay>>;
-  festives: Festive[];
-  oldFestives: MetadataDay[] = [];
-  newFestives: MetadataDay[] = [];
-  selectedCenter: number;
 
-
-  constructor(public dialogConf: DynamicDialogConfig,
-              private calendarService: CalendarService, 
-              public ref: DynamicDialogRef) {
+  constructor() {
     this.calendars = new Map();
-    
 
-    /*
     const actualYear = new Date().getFullYear() + 1;
     for (let i = actualYear + 1; i >= 2022; i--) {
-      this.years.push(
-        new DropdownEntry({ code: i.toString(), name: i.toString() })
-      );
-    }
-
-    */
-
-    const actualYear = new Date().getFullYear() ;
-    for (let i = actualYear -2; i <= actualYear + 2; i++) {
       this.years.push(
         new DropdownEntry({ code: i.toString(), name: i.toString() })
       );
@@ -68,7 +47,7 @@ export class CalendarComponent{
         id: 1,
         name: 'Festivo',
         absence: true,
-        color: '#aae3ff',
+        color: '#aaaaff',
       }),
       new ScheduleType({
         id: 2,
@@ -97,7 +76,6 @@ export class CalendarComponent{
     this.selectedType = this.scheduleTypes[0];
     this.selectedCalendar = this.generateDefaultCalendar();
   }
-
   onChangeYear(): void {
     this.selectedCalendar = this.generateDefaultCalendar();
   }
@@ -110,11 +88,6 @@ export class CalendarComponent{
     const metadataDay = new Map<String, MetadataDay>();
     const normalDay = this.scheduleTypes[1];
 
-    if(this.dialogConf.data.festivesData != undefined){
-      this.festives = this.dialogConf.data.festivesData;
-      this.selectedCenter = this.festives[0].centerId;
-    }
-
     for (let month = 0; month < 12; month++) {
       for (let day = 1; day <= 31; day++) {
         const date = new Date(parseInt(this.selectedYearAux.code), month, day);
@@ -123,22 +96,6 @@ export class CalendarComponent{
 
           let type = normalDay;
 
-          const isFestive = this.festives.some(festive => {
-            let date = String(festive.date).slice(-2);
-            let festiveDay = parseInt(date, 10);
-            if (festive.year === Number(this.selectedYearAux.code) &&
-                festive.month === month + 1 &&
-                festiveDay === day ){              
-              return true;
-            }
-          
-            return false;
-          });
-
-          if (isFestive) {
-            type = this.scheduleTypes.find(type => type.name === 'Festivo');
-          }
-          
           if (isWeekend) {
             type = this.scheduleTypes.find(
               (type) => type.name === 'Fin de semana'
@@ -160,74 +117,5 @@ export class CalendarComponent{
     }
 
     return metadataDay;
-  }
-
-  selectDate(day:MetadataDay){
-
-    if(day && day.type.name != 'Fin de semana'){
-      const isFestive = day.type.name === "Festivo";
-
-      const newDate = new Date();
-      newDate.setDate(day.day);
-      newDate.setMonth(day.month+1);
-      newDate.setFullYear(day.year);
-      if(isFestive){
-        day.originalType = day.type;
-        day.type = this.scheduleTypes.find(type => type.name === 'Jornada normal (8h 25min)');
-        if(this.festives.some(festive => this.isSameDate(festive, newDate)) &&
-          !this.oldFestives.includes(day)
-        ){
-          this.oldFestives.push(day);
-        }else{
-          //Comprobar que el nuevo dia festivo no esté en el array de nuevos dias festivos
-          //Si lo está, se elimina
-          if(this.newFestives.includes(day)){
-            const index = this.newFestives.indexOf(day);
-            this.newFestives.splice(index,1);
-          }
-        }
-      }else{
-        day.originalType = day.type;
-        day.type = this.scheduleTypes.find(type => type.name === 'Festivo');
-
-        //Comprobar si es un dia festivo que ya existia pero se habia puesto en la cola de eliminación
-        if(this.festives.some(festive => this.isSameDate(festive, newDate)) &&
-          this.oldFestives.includes(day)
-        ){
-          //Quitar del array de eliminación el dia que ya estaba dado de alta en la BD
-          const index = this.oldFestives.indexOf(day);
-          this.oldFestives.splice(index,1);
-        }else{
-          if(!this.newFestives.includes(day)){
-            this.newFestives.push(day);
-          }
-        }
-      }
-
-      this.clickEvent.emit(day);
-    }
-  }
-
-  close(){
-    console.log("Nuevos festivos: ", this.newFestives);
-    console.log("Festivos antiguos a eliminar: ", this.oldFestives);
-    this.ref.close();
-  }
-
-  update():void{
-    this.calendarService.update(this.newFestives);
-    this.calendarService.delete(this.oldFestives);
-  }
-
-  private isSameDate(festive: Festive, newDate: Date): boolean {
-    let date = String(festive.date).slice(-2);
-    let festiveDay = parseInt(date, 10);
-    if (festive.year === newDate.getFullYear() &&
-        festive.month === newDate.getMonth() &&
-        festiveDay === newDate.getDate() ){              
-          return true;
-    }
-
-    return false;
   }
 }
