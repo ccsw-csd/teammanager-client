777e19bd9f21f6d713be9880d7ccf771a1d35415
ivanmr14 <imitro@alumni.uv.es>
2024-02-28 13:47:09 +0100

    Avances edición calendar

--- src/app/calendar/calendar.service.ts
+++ src/app/calendar/calendar.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Detail } from '../forecast/model/Detail';
 import { environment } from 'src/environments/environment';
+import { PersonAbsence } from './views/model/PersonAbsence';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,14 @@ export class CalendarService {
   getUserDetails(year: string):Observable<Detail>{
     const url = `${environment.server}/person/?year=${year}`;
     return this.http.get<Detail>(url);
+  }
+
+  save(vacations: PersonAbsence[]):Observable<PersonAbsence[]>{
+    return this.http.put<PersonAbsence[]>(environment.server + "/person_absence/",vacations);  
+  }
+
+  delete(idVacation: string):Observable<void>{
+    const url = `${environment.server}/person_absence/${idVacation}`;
+    return this.http.delete<void>(url);
   }
 }
--- src/app/calendar/views/calendar/calendar.component.ts
+++ src/app/calendar/views/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, Output, EventEmitter } from '@angular/core';
 import { ScheduleType } from '../model/schedule-type';
 import { MetadataDay } from '../model/metadata-day';
 import { DropdownEntry } from '../model/dropdown-entry';
@@ -10,6 +10,8 @@ import { AuthService } from 'src/app/core/services/auth.service';
 import { CalendarService } from '../../calendar.service';
 import { finalize } from 'rxjs';
 import { Detail } from '../model/Detail';
+import { PersonAbsence } from '../model/PersonAbsence';
+import { Person } from '../model/Person';
 
 
 
@@ -22,6 +24,8 @@ import { Detail } from '../model/Detail';
 export class CalendarComponent {
   @ViewChild('calendarsDiv') calendarsDiv: ElementRef;
 
+  @Output() clickEvent = new EventEmitter<MetadataDay>();
+
   //ATRIBUTOS
 
  
@@ -31,10 +35,14 @@ export class CalendarComponent {
   selectedYearAux: DropdownEntry;
   selectedCalendar: Map<String, MetadataDay>;
   detail: Detail;
+  withPon: boolean;
 
   years: DropdownEntry[] = [];
   workingDaysPerMonth: Number[] = [];
   calendars: Map<String, Map<String, MetadataDay>>;
+
+  oldVacations: MetadataDay[] = [];
+  newVacations: MetadataDay[] = [];
 
   constructor(private authService: AuthService,
     private calendarService: CalendarService,
@@ -239,12 +247,131 @@ export class CalendarComponent {
   loadUserDetails(){
     this.calendarService.getUserDetails(this.selectedYearAux.code).pipe(
       finalize(() => {
+        this.withPon = this.detail.person.withPON;
         this.selectedCalendar = this.generateDefaultCalendar();
       })
       ).subscribe({
       next: (res: Detail) => {
-        this.detail = res;
+        this.detail = res;     
       },
     });   
+  }
+
+  isButtonDisabled(): boolean{
+    let buttonDisabled = true;
+
+    if((this.oldVacations.length != 0) || (this.newVacations.length != 0)){
+      buttonDisabled = false;
+    }
+    return buttonDisabled;
+  }
+
+  isWithPonActive(): boolean{
+    let active = true;
+
+    if(this.withPon == false){
+      active = false;
+    }
+    return active;
+  }
+
+  selectDate(day:MetadataDay){
+
+    if(day){
+      const isVacation = day.type.name === "Vacation";
+      const newDate = new Date();
+      newDate.setDate(day.day);
+      newDate.setMonth(day.month);
+      newDate.setFullYear(day.year);
+
+      if(isVacation){
+        if(day.originalType == this.scheduleTypes.find(type => type.name === 'Fin de semana')){
+          day.type = this.scheduleTypes.find(type => type.name === 'Fin de semana');
+        }else{
+          day.type = this.scheduleTypes.find(type => type.name === 'Jornada normal (8h 25min)');
+        }
+        
+        if(this.detail.absences.some(vacation => this.isSameDate(vacation, newDate)) &&
+          !this.oldVacations.includes(day)
+        ){
+          this.oldVacations.push(day);
+        }else{
+          //Comprobar que el nuevo dia de vacaciones no esté en el array de nuevos dias de vacaciones
+          //Si lo está, se elimina
+          if(this.newVacations.includes(day)){
+            const index = this.newVacations.indexOf(day);
+            this.newVacations.splice(index,1);
+          }
+        }
+      }else{
+        
+        day.type = this.scheduleTypes.find(type => type.name === 'Vacation');
+
+        //Comprobar si es un dia de vacaciones que ya existia pero se habia puesto en la cola de eliminación
+        if(this.detail.absences.some(vacation => this.isSameDate(vacation, newDate)) &&
+          this.oldVacations.includes(day)
+        ){
+          //Quitar del array de eliminación el dia que ya estaba dado de alta en la BD
+          const index = this.oldVacations.indexOf(day);
+          this.oldVacations.splice(index,1);
+        }else{
+          if(!this.newVacations.includes(day)){
+            this.newVacations.push(day);
+          }
+        }
+      }
+
+      this.clickEvent.emit(day);
+    }
+  }
+
+  save():void{
+    if(this.newVacations.length > 0){
+      const vacation = new PersonAbsence();
+      let vacations: PersonAbsence[] = [];
+      for (const md of this.newVacations) {
+        vacation.id = '_P_' + this.detail.person.id.toString();
+        vacation.person = this.detail.person;
+        vacation.year = md.year;
+        vacation.month = md.month;
+        vacation.date = this.formatDate(md);
+        vacation.type = 'P';
+        vacation.absence_type = 'VAC';
+        vacations.push(vacation);    
+      }
+
+      this.calendarService.save(vacations).subscribe();
+    }
+
+    if(this.oldVacations.length > 0){
+      for (const md of this.oldVacations) {
+        let id = this.formatDate(md).toString() + '_P_' + this.detail.person.id.toString();
+        this.calendarService.delete(id).subscribe();
+      }
+    }
+
+    setTimeout(() => {
+      this.loadUserDetails();
+    }, 100);
+    
+  }
+
+  formatDate(metadataDay:MetadataDay):Date{
+    let date: Date;
+    date = new Date(""+metadataDay.year+"-"+(metadataDay.month+1)+"-"+metadataDay.day);
+    return date;
+  }
+
+  private isSameDate(absence: PersonAbsence, newDate: Date): boolean {
+    let date = String(absence.date).slice(-2);
+    let vacationDay = parseInt(date, 10);
+
+    if (absence.year === newDate.getFullYear() &&
+        absence.month -1 === newDate.getMonth() &&
+        vacationDay === newDate.getDate() ){              
+          return true;
+    }
+
+    return false;
   }
 }
